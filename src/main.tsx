f8bed5334eebde5753e94f88a2594ec5a7cd1e6c
schmidtke-phil <phil.schmidtke@telekom.de>
2022-11-03 11:28:14 +0100

    Add Venly Connect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { VenlyConnect } from '@venly/connect/dist/src/connect/connect'
+
+// Initialize the Venly Widget
+const venlyConnect = new VenlyConnect('Testaccount', {
+  environment: "staging",
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

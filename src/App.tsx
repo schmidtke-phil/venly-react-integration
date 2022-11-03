d7d9ada519e90b796219209926e4a2974e380424
schmidtke-phil <phil.schmidtke@telekom.de>
2022-11-03 12:58:59 +0100

    Create Example Function Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { VenlyConnect } from '@venly/connect'
 import React from 'react'
+import Functions from './Functions';
 
 type AppProps = {
   venlyConnect: VenlyConnect;
@@ -10,10 +11,13 @@ function App({venlyConnect}: AppProps) {
   return (
     <div>
       <div className='border-b-2 border-indigo-700 rounded-b-lg shadow-xl py-2'>
-        <h1 className='text-center'>Venly React Integration Demo</h1>
+        <h1 className='text-center text-xl font-bold'>Venly React Integration Demo</h1>
+      </div>
+      <div className='mt-4 mx-4 flex'>
+        <button className='mx-auto border border-indigo-700 rounded-xl py-2 px-4 hover:scale-105 transition-all ease-in-out 0.7s'>Venly Wallet Login</button>
       </div>
       <div className='mt-4 mx-4'>
-        <button className=''>Venly Wallet Login</button>
+        <Functions />
       </div>
     </div>
   )
---
+++ src/Functions.tsx
@@ -0,0 +1,18 @@
+export default function Functions() {
+    return (
+        <div>
+            <h2 className="text-center text-xl">Example Functions</h2>
+            <div className="flex mx-auto">
+                <div className="w-1/4 border border-indigo-700 rounded-xl">
+                    <h3 className="text-center">Manage Wallets</h3>
+                    <button className="flex mx-auto">Test Function</button>
+                    <div className="mt-4 p-4">
+                        <div className="w-full h-full bg-indigo-700 rounded-xl p-1">
+                            JSON Stringify res
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
